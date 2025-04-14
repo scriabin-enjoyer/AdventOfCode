029cb83036b926f83a9fe786c9c9c51fc84fb8bc
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2025-04-14 17:43:57 -0400

    2015/day3: Implement pt1 driver code (draft)

--- 2015-C/day3/pt1.c
+++ 2015-C/day3/pt1.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+/*#include <ctype.h>*/
+/*#include <stdlib.h>*/
+
+void g_add_node(int[2]);
+int g_node_count(void);
+void update_pos(int[2], char);
 
 int main(void)
 {
-        int char_count = system("wc --bytes input.txt");
-        printf("%d\n", char_count);
+        FILE *fp;
+        int pos[2] = { 0 };
+        char move;
+
+        // File reading
+        fp = fopen("input.txt", "r");
+        if (fp == NULL) {
+                perror("Can't read file. Exiting.");
+                return 1;
+        }
+
+        // Build Graph
+        g_add_node(pos);
+        while ((move = fgetc(fp)) != EOF) {
+                update_pos(pos, move);
+                g_add_node(pos);
+        }
+
+        fclose(fp);
+
+        // success
+        printf("|G| = %d\n", g_node_count());
         return 0;
 }
